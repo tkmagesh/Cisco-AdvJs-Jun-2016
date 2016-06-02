4d627984dc19f3f78f82fe483712612b7d93db9a
Magesh <tkmagesh77@gmail.com>
2016-06-02 16:26:50 +0530

    Day-02 Complete

--- Day-02/05-ES6/es6featuresf.js
+++ Day-02/05-ES6/es6featuresf.js
@@ -11,3 +11,24 @@ class Employee{
       return obj instanceof Employee;
    }
 }
+
+class FulltimeEmployee extends Employee{
+  constructor(id,name,salary,benefits){
+    super(id, name, salary);
+    this.benefits = benefits;
+  }
+}
+
+var Spinner = (function(){
+  var countSymbol = Symbol();
+  function Spinner(){
+    this[countSymbol] = 0;
+  }
+  Spinner.prototype.up = function(){
+    return ++this[countSymbol];
+  };
+  Spinner.prototype.down = function(){
+    return --this[countSymbol];
+  }
+  return Spinner;
+})();
